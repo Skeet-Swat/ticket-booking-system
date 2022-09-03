5a313b76255d2f61f1d9be78601e8ac9bcc968d9
Aman Kumar <aman.kumar2@citiustech.com>
2022-09-03 20:47:31 +0530

    API Call & Home Page Design

--- movie-ticket-booking-system/src/app/app.component.ts
+++ movie-ticket-booking-system/src/app/app.component.ts
@@ -1,10 +1,33 @@
+import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+// import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  data: any;
+  img : any = [];
+  updatedData: any;
+  constructor(private _service: ApiService){
+   this._service.callApi().subscribe((data: any)=> {
+    // console.log("data", data);
+    this.updatedData = data;
+
+    console.log("poster", data.Search[0].Poster);
+    // this.posterImg = data;
+    console.log("updated Data", this.updatedData)
+
+    for(let item of this.updatedData.Search){
+      this.img.push(item.Poster)
+    }
+    console.log("All Img",this.img);
+
+   })
+   
+  }
+  
   title = 'movie-ticket-booking-system';
 }
